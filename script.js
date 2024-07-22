5f0422c39a319ee6cf1dab3bb25c92e7ed41a9f3
Shegofa <shogofadeveloper12@gmail.com>
2024-07-22 14:02:04 +0400

    home section linters error silved

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-let sections = document.querySelectorAll("section");
-let navlinks = document.querySelectorAll("header nav a ");
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const sections = document.querySelectorAll('section');
+const navlinks = document.querySelectorAll('header nav a ');
 
 window.onscroll = () => {
   sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute;
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute;
 
     if (top >= offset && top < offset + height) {
       navlinks.forEach((links) => {
-        links.classList.remove("active");
+        links.classList.remove('active');
         document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
+          .querySelector(`header nav a[href*=' ${id}']`)
+          .classList.add('active');
       });
     }
   });
 };
 
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 });
 
 // socia media dotted part //////////////////////////////////////////////////////////
-let navigation = document.querySelector(".navigation");
-navigation.onclick = function () {
-  navigation.classList.toggle("active");
-};
-//////////////////////////////////////////////////////////////////////////////////////
+const navigation = document.querySelector('.navigation');
+navigation.addEventListener('click',  function () {
+  navigation.classList.toggle('active');
+});
