393cf197b17fdad048b1aa127d8f0bbd8e813c64
Shegofa <shogofadeveloper12@gmail.com>
2024-07-22 13:08:17 +0400

    home section added

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let hamburgerMenu = document.getElementById("hamburger-menu");
-let mobileMenu = document.getElementById("mobile-menu");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a ");
 
-hamburgerMenu.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
-});
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute;
 
-document.querySelectorAll(".menu a").forEach((link) => {
-  link.addEventListener("click", () => {
-    document.querySelector(".menu").classList.add("hidden");
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
   });
+};
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 });
+
+// socia media dotted part //////////////////////////////////////////////////////////
+let navigation = document.querySelector(".navigation");
+navigation.onclick = function () {
+  navigation.classList.toggle("active");
+};
+//////////////////////////////////////////////////////////////////////////////////////
