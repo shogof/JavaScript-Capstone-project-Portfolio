5ca40cddb8d4727099c88af54538bccff4666291
Shegofa <shogofadeveloper12@gmail.com>
2024-08-03 10:31:43 +0400

    Home section linters solved

--- scripts/aboutMe.js
+++ scripts/aboutMe.js
@@ -1,7 +1,7 @@
-const number = document.getElementById("number");
-const number1 = document.getElementById("number1");
-const number2 = document.getElementById("number2");
-const number3 = document.getElementById("number3");
+const number = document.getElementById('number');
+const number1 = document.getElementById('number1');
+const number2 = document.getElementById('number2');
+const number3 = document.getElementById('number3');
 
 let counter = 0;
 let counter1 = 0;
@@ -39,45 +39,45 @@ setInterval(() => {
 
 // slider part
 
-const sliderContainer = document.getElementById("slid");
+const sliderContainer = document.getElementById('slid');
 
 const sliderData = [
   {
-    src: "img/elevate.jpg",
-    alt: "Image 1",
-    link: "https://github.com/",
+    src: 'img/elevate.jpg',
+    alt: 'Image 1',
+    link: 'https://github.com/',
   },
   {
-    src: "img/html cer.png",
-    alt: "Image 2",
-    link: "https://example.com/image2",
+    src: 'img/html cer.png',
+    alt: 'Image 2',
+    link: 'https://example.com/image2',
   },
-  { src: "img/elevate.jpg", alt: "Image 3", link: "#" },
-  { src: "img/ayla.jpg", alt: "Image 4", link: "https://example.com/image4" },
-  { src: "img/js.jpg", alt: "Image 5", link: "https://example.com/image5" },
+  { src: 'img/elevate.jpg', alt: 'Image 3', link: '#' },
+  { src: 'img/ayla.jpg', alt: 'Image 4', link: 'https://example.com/image4' },
+  { src: 'img/js.jpg', alt: 'Image 5', link: 'https://example.com/image5' },
   {
-    src: "img/elevate.jpg",
-    alt: "Image 6",
-    link: "https://example.com/image6",
+    src: 'img/elevate.jpg',
+    alt: 'Image 6',
+    link: 'https://example.com/image6',
   },
   {
-    src: "img/html cer.png",
-    alt: "Image 7",
-    link: "https://example.com/image7",
+    src: 'img/html cer.png',
+    alt: 'Image 7',
+    link: 'https://example.com/image7',
   },
 ];
 
 function createSlider() {
   sliderData.forEach((item) => {
-    const imgDiv = document.createElement("div");
-    imgDiv.classList.add("img");
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('img');
 
-    const linkElement = document.createElement("a");
+    const linkElement = document.createElement('a');
     linkElement.href = item.link;
-    linkElement.innerHTML = "connect";
-    linkElement.classList.add("linkm");
+    linkElement.innerHTML = 'connect';
+    linkElement.classList.add('linkm');
 
-    const imgElement = document.createElement("img");
+    const imgElement = document.createElement('img');
     imgElement.src = item.src;
     imgElement.alt = item.alt;
 
@@ -89,10 +89,10 @@ function createSlider() {
 
 createSlider();
 
-const slider = document.querySelector(".slider");
-const btns = document.querySelectorAll(".btn");
-const slides = document.querySelectorAll(".img");
-const options = document.querySelectorAll(".option");
+const slider = document.querySelector('.slider');
+const btns = document.querySelectorAll('.btn');
+const slides = document.querySelectorAll('.img');
+const options = document.querySelectorAll('.option');
 
 let index = 1;
 let opIndex = 0;
@@ -100,19 +100,19 @@ const size = slides[index].clientWidth;
 
 function update() {
   slider.style.transform = `translateX(${-size * index}px)`;
-  options.forEach((option) => option.classList.remove("colored"));
-  options[opIndex].classList.add("colored");
+  options.forEach((option) => option.classList.remove('colored'));
+  options[opIndex].classList.add('colored');
 }
 
 update();
 
 function slide() {
-  slider.style.transition = "transform .5s ease-in-out";
+  slider.style.transition = 'transform .5s ease-in-out';
   update();
 }
 
 function btnCheck() {
-  if (this.id === "prev") {
+  if (this.id === 'prev') {
     index -= 1;
     if (opIndex === 0) {
       opIndex = 4;
@@ -132,24 +132,24 @@ function btnCheck() {
 }
 
 function optionFunc() {
-  const i = Number(this.getAttribute("op-index"));
+  const i = Number(this.getAttribute('op-index'));
   opIndex = i;
   index = i + 1;
 
   slide();
 }
 
-slider.addEventListener("transitionend", () => {
-  if (slides[index].id === "first") {
-    slider.style.transition = "none";
+slider.addEventListener('transitionend', () => {
+  if (slides[index].id === 'first') {
+    slider.style.transition = 'none';
     index = slides.length - 2;
     slider.style.transform = `translateX(${-size * index}px)`;
-  } else if (slides[index].id === "last") {
-    slider.style.transition = "none";
+  } else if (slides[index].id === 'last') {
+    slider.style.transition = 'none';
     index = 1;
     slider.style.transform = `translateX(${-size * index}px)`;
   }
 });
 
-btns.forEach((btn) => btn.addEventListener("click", btnCheck));
-options.forEach((option) => option.addEventListener("click", optionFunc));
+btns.forEach((btn) => btn.addEventListener('click', btnCheck));
+options.forEach((option) => option.addEventListener('click', optionFunc));
