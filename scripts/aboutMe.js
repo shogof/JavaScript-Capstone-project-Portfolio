05f0b5835a9b22e048215d2ee4e30df2019e339a
Shegofa <shogofadeveloper12@gmail.com>
2024-08-03 09:43:05 +0400

    about section linter errors solved

--- scripts/aboutMe.js
+++ scripts/aboutMe.js
@@ -1,35 +1,35 @@
-let number = document.getElementById("number");
-let number1 = document.getElementById("number1");
-let number2 = document.getElementById("number2");
-let number3 = document.getElementById("number3");
+const number = document.getElementById("number");
+const number1 = document.getElementById("number1");
+const number2 = document.getElementById("number2");
+const number3 = document.getElementById("number3");
 
 let counter = 0;
 let counter1 = 0;
 let counter2 = 0;
 let counter3 = 0;
 setInterval(() => {
-  if (counter == 90) {
+  if (counter === 90) {
     clearInterval();
   } else {
     counter += 1;
     number.innerHTML = counter + "%";
   }
 
-  if (counter1 == 95) {
+  if (counter1 === 95) {
     clearInterval();
   } else {
     counter1 += 1;
     number1.innerHTML = counter1 + "%";
   }
 
-  if (counter2 == 80) {
+  if (counter2 === 80) {
     clearInterval();
   } else {
     counter2 += 1;
     number2.innerHTML = counter2 + "%";
   }
 
-  if (counter3 == 98) {
+  if (counter3 === 98) {
     clearInterval();
   } else {
     counter3 += 1;
@@ -94,17 +94,17 @@ const btns = document.querySelectorAll(".btn");
 const slides = document.querySelectorAll(".img");
 const options = document.querySelectorAll(".option");
 
-var index = 1;
-var op_index = 0;
-var size = slides[index].clientWidth;
-
-update();
+let index = 1;
+let opIndex = 0;
+let size = slides[index].clientWidth;
 
 function update() {
   slider.style.transform = "translateX(" + -size * index + "px)";
   options.forEach((option) => option.classList.remove("colored"));
-  options[op_index].classList.add("colored");
+  options[opIndex].classList.add("colored");
 }
+
+update();
 
 function slide() {
   slider.style.transition = "transform .5s ease-in-out";
@@ -113,18 +113,18 @@ function slide() {
 
 function btnCheck() {
   if (this.id === "prev") {
-    index--;
-    if (op_index === 0) {
-      op_index = 4;
+    index -= 1;
+    if (opIndex === 0) {
+      opIndex = 4;
     } else {
-      op_index--;
+      opIndex -= 1;
     }
   } else {
-    index++;
-    if (op_index === 4) {
-      op_index = 0;
+    index += 1;
+    if (opIndex === 4) {
+      opIndex = 0;
     } else {
-      op_index++;
+      opIndex += 1;
     }
   }
 
@@ -132,8 +132,8 @@ function btnCheck() {
 }
 
 function optionFunc() {
-  let i = Number(this.getAttribute("op-index"));
-  op_index = i;
+  const i = Number(this.getAttribute("op-index"));
+  opIndex = i;
   index = i + 1;
 
   slide();
@@ -146,7 +146,7 @@ slider.addEventListener("transitionend", () => {
     slider.style.transform = "translateX(" + -size * index + "px)";
   } else if (slides[index].id === "last") {
     slider.style.transition = "none";
-    index = 1;
+    index = 1;  
     slider.style.transform = "translateX(" + -size * index + "px)";
   }
 });
